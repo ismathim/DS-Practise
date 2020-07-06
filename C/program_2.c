a6fac7d3cdf46f1e6be48d4076a55f0b63433889
Ismath <ismathim@gmail.com>
2020-07-06 17:38:36 +0400

    Int, Float and Long

---
+++ C/program_2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+  int i=10;
+  long l= 25L;
+  unsigned long ul=2500UL;
+  printf("Unsigned long is %lu \n",ul);
+  float f=1.25f;
+  long double ld= 22.34L;
+  printf("Float %f and long double %Lg",f,ld);
+  return 0
+  
+  printf("The result: Unsigned long is 2500
+                      Float 1.250000 aand long double 22.34% ");
+}
+
+
+
+
