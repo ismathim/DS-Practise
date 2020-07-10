95717f330a7250f862c57045e34c514dd6ee4b46
Ismath <ismathim@gmail.com>
2020-07-10 23:35:54 +0400

    Create program_5.c

---
+++ C/program_5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int val=2;
+    printf("val=%i \n",val);
+
+    printf("val=%i \n",val++);
+
+    printf("val=%i \n",val--);
+
+    printf("val=%i \n",--val);
+}
+    
+ 
